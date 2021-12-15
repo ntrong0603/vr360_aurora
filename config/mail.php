2dcb51ac2bfe659cb61e6a79003d6d3cb50fa992
lengoctrong <ntrong0603.dgt@gmail.com>
2021-12-15 13:50:46 +0700

    fix error send mail

--- config/mail.php
+++ config/mail.php
@@ -43,6 +43,13 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
+            'stream' => [
+                'ssl' => [
+                    'allow_self_signed' => env('MAIL_SSLOPTIONS_ALLOW_SELF_SIGNED', false),
+                    'verify_peer' => env('MAIL_SSLOPTIONS_VERIFY_PEER', true),
+                    'verify_peer_name' => env('MAIL_SSLOPTIONS_VERIFY_PEER_NAME', true),
+                ],
+            ],
         ],
 
         'ses' => [
